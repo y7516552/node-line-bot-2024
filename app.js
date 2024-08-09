b8f5075d6760b26e1f04667b43bd58b7c24f333c
Steven Zhong <89566389+y7516552@users.noreply.github.com>
2024-08-09 18:00:00 +0800

    install express

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
-var linebot = require('linebot');
-
 require('dotenv').config();
 
-var bot = linebot({
+const express = require('express');
+const linebot = require('linebot');
+
+const app = express();
+
+const bot = linebot({
   channelId: process.env.CHANNEL_ID,
   channelSecret: process.env.CHANNEL_SECRET,
   channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN
@@ -11,12 +14,14 @@ var bot = linebot({
 bot.on('message', function (event) {
     var replyMsg = `Hello你剛才說的是:${event.message.text}`;
     event.reply(replyMsg).then(function (data) {
+        console.log("event",event)
+        console.log("data",data)
     }).catch(function (error) {
-
+        console.log("error",error)
     });
 });
 
-bot.get("/", (req, res) => {
+app.get("/", (req, res) => {
     res.sendStatus(200);
 });
 
@@ -24,3 +29,7 @@ bot.get("/", (req, res) => {
 bot.listen('/linewebhook', 3000, function () {
     console.log('[BOT已準備就緒]');
 });
+
+app.listen(3001, () => {
+    console.log(`listening on ${3001}`);
+})

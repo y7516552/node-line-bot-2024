dd6399314199fdd7741d7a8ffa337edf5a07da40
Steven Zhong <89566389+y7516552@users.noreply.github.com>
2024-08-09 15:31:50 +0800

    0809
    set up

---
+++ app.js
@@ -0,0 +1,25 @@
+var linebot = require('linebot');
+
+// 用於辨識Line Channel的資訊
+var bot = linebot({
+  channelId: process.env.CHANNEL_ID,
+  channelSecret: process.env.CHANNEL_SECRET,
+  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN
+});
+
+bot.on('message', function (event) {
+    // event.message.text是使用者傳給bot的訊息
+    // 準備要回傳的內容
+    var replyMsg = `Hello你剛才說的是:${event.message.text}`;
+    // 透過event.reply(要回傳的訊息)方法將訊息回傳給使用者
+    event.reply(replyMsg).then(function (data) {
+        // 當訊息成功回傳後的處理
+    }).catch(function (error) {
+        // 當訊息回傳失敗後的處理
+    });
+});
+
+// Bot所監聽的webhook路徑與port
+bot.listen('/linewebhook', 3000, function () {
+    console.log('[BOT已準備就緒]');
+});
